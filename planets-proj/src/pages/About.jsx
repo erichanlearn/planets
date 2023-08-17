fa42a00f4f698e9cb7f6add56bebc9443615221b
Eric Han <eric.han@duke.edu>
2023-08-17 23:56:37 +0000

    planets api integration, add image api and habitability function

--- planets-proj/src/pages/About.jsx
+++ planets-proj/src/pages/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import '../index.css'
 const About = () => {
   return (
-    <div>
+    <div className = "aboutpage">
       About
     </div>
   );
--- planets-proj/src/pages/Search.jsx
+++ planets-proj/src/pages/Search.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
-import '../index.css'
+import React, { useState } from 'react';
+import axios from 'axios';
+import '../index.css';
+
 const Search = () => {
-  return (
-    <div>
+  const [data, setData] = useState('');
+  const [planetName, setPlanetName] = useState('');
+
+  const searchPlanetName = (event) => {
+    if (event.key === 'Enter') {
+      const url = `https://api.api-ninjas.com/v1/planets?name=${encodeURIComponent(planetName)}`;
       
+      axios.get(url, {
+        headers: {
+          'X-Api-Key': 'Xd3KueehnsMaroIP3p0SNw==Y5NKsWTw2Y5QSHPy'
+        }
+      })
+      .then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error('Request failed:', error);
+      });
+    }
+  };
+
+  return (
+    <div className="searchpage">
+      <div className="searchbar">
+        <h1>blah</h1>
+        <input
+          value={planetName}
+          onKeyDown={searchPlanetName}
+          onChange={(event) => setPlanetName(event.target.value)}
+          placeholder='Enter planet name'
+          type="text"
+        />
+      </div>
     </div>
   );
 }
